6e2224329df66bf79b4ada3386b2a2c3bc56a061
pe200012 <1326263755@qq.com>
2018-08-14 20:50:53 +0800

    add main file

---
+++ src/bin/main.rs
@@ -0,0 +1,7 @@
+extern crate messager_server;
+use messager_server::my_channel::MyChannel;
+
+fn main() {
+    // let ch: MyChannel = MyChannel::new("Channel 3".to_string(), "0.0.0.0:12110", 3);
+    // ch.run();
+}
